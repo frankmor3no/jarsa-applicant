e4507c5a79e3f2359eb68c8e308056f0deed4bca
frankmor3no <43287969+frankmor3no@users.noreply.github.com>
2019-07-04 01:27:27 -0500

    Python2 Technical Test

--- calculator.py
+++ calculator.py
@@ -7,10 +7,15 @@ class CalculatorClass(object):
     """
     Your class documentation here
     """
-
+    self.num_list = num_list
+    
     def sum(self, num_list):
         """
         Your method documentation here
         """
         # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        suma = 0
+    for i in num_list:
+        suma = suma + i
+
+        return suma
