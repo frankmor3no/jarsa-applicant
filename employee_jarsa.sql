79de3537048db9bed15cbf4391b4be1b4193f345
frankmor3no <43287969+frankmor3no@users.noreply.github.com>
2019-07-03 22:19:32 -0500

    Add files via upload

--- employee_jarsa.sql
+++ employee_jarsa.sql
@@ -4,12 +4,35 @@
 --       Consider add ';' at end sentence.
 
 CREATE TABLE employee (
+    id VARCHAR(5),
+    firstname VARCHAR(50),
+    lastname VARCHAR(50)
 );
 
 CREATE TABLE employee_department (
+    id VARCHAR(5),
+    names VARCHAR(20),
+    descript VARCHAR(140)
 );
 
-CREATE TABLE employee_hobby (
-);
+--CREATE TABLE employee_hobby ();
+
+-- Tabla Empleado
+INSERT INTO employee (id, firstname, lastname) 
+VALUES (1, 'Alejandra', 'Moreno'), 
+(2, 'Daniel', 'Cruz'), 
+(3, 'MariaJ', 'Sifuentes'),
+(4, 'Karla', 'Garcia'),
+(5, 'Agustín', 'Lara'),
+(6, 'Peter', 'Child');
+
+-- Tabla Departamentos
+INSERT INTO employee_department (id, names, descript) 
+VALUES (1, 'Gerencia', 'Principal'), 
+(2, 'Comedor', 'Lugar para descanso'), 
+(3, 'Vigilancia', 'Seguridad de la empresa'),
+(4, 'Almacen', 'Recibimiento de inventario'),
+(5, 'Contabilidad', 'Llevar a cabo la cuenta monetaria'),
+(6, 'Sistemas', 'Encargados del sistema principal');
 
 -- ...
